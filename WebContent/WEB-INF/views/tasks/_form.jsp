a3fb802355af589d309455c6b2565495e5db2ee6
yuna.techacademy <taro.kirameki@techacademy.jp>
2021-02-21 15:37:18 +0900

    Modify Index/NewServlet

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,4 @@
+<label>タスク<br />
+<input type="text" name="title" value="${task.content}" />
+</label>
+<br /><br />
--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,19 +1,23 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Task;
+
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +26,20 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token",request.getSession().getId());
+
+        request.setAttribute("task",new Task());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
+
+
+
+
+    }
 
 }
