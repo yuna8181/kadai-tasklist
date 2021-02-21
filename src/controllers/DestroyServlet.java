19d0939a6a80fac6000a04d72525f65195b0eb1b
yuna.techacademy <taro.kirameki@techacademy.jp>
2021-02-22 00:05:06 +0900

    Add DestroyServlet.java

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DButil;
+
+/**
+ * Servlet implementation class DestroyServlet
+ */
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DestroyServlet() {
+        super();
+    }
+
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DButil.createEntityManager();
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            em.getTransaction().begin();
+            em.remove(t);
+            em.getTransaction().commit();
+            em.close();
+
+            request.getSession().removeAttribute("message_id");
+
+            response.sendRedirect(request.getContextPath() + "/index");
+
+
+        }
+
+    }
+
+}
